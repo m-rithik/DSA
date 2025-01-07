1abe0d52ab2c72e16b750b6a7905841fe77f3146
Rithik <162104114+m-rithik@users.noreply.github.com>
2025-01-07 23:39:02 +0530

    commit

---
+++ Day 9/1480leetcode.cpp
@@ -0,0 +1,18 @@
+//1480. Running Sum of 1d Array
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n=nums.size();
+        int temp=0;
+        vector<int> ans(n);
+        for(int i=0;i<n;i++){
+            temp+=nums[i];
+            ans[i]=temp;
+        }
+        return ans;
+    }
+};
+
+/*
+nothing re you just use a for loop and as u make the sum add it to the array*/
