c96697bc525c246cbea4f874a939329dc099e6bc
m-rithik <rithikmeedinti.stu@gmail.com>
2025-02-25 22:05:39 +0530

    commit

--- CP/twoq.c
+++ CP/twoq.c
@@ -1,77 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+bool is_bit_set(long long num, int bit_pos) {
+    return (num & (1LL << bit_pos)) != 0;
+}
 
 void solve_test_case() {
     int n;
     long long x;
     scanf("%d %lld", &n, &x);
+    
+
     if (n == 1) {
         printf("%lld\n", x);
-        return;
-    }if (n == 2 && x == 3) {
-        printf("0 3\n");
         return;
     }
     
     long long *a = (long long*)malloc(n * sizeof(long long));
-    for (int i = 0; i < n; i++) {
-        a[i] = i;
-    }
     
- 
-    long long current_or = 0;
-    for (int i = 0; i < n; i++) {
-        current_or |= a[i];
-    }
     
-
-    if (current_or != x) {
-        if (n >= 7 && x == 7) {
-
-            if (n == 7) {
-                printf("6 0 3 4 1 2 5\n");
-            } else {
-                printf("7 0 6 1 5 2 4 3\n");
-            }
-        } else if (n >= 5 && x == 7) {
-          
-            printf("4 1 3 0 2\n");
-        } else if (n >= 3 && x == 52) {
-      
-            printf("0 52 0\n");
-        } else if (n >= 9 && x == 11) {
-            // Special case for n=9, x=11
-            printf("0 1 8 3 0 9 11 2 10\n");
-        } else if (n >= 6 && x == 15) {
-            // Special case for n=6, x=15
-            printf("4 0 3 8 1 2\n");
-        } else {
-            // Generic approach: make the first element significant
-            a[0] = x;
-            
-            // Fill remaining positions with values that don't affect OR
-            int pos = 1;
-            for (int i = 0; pos < n && i < 30; i++) {
-                if ((i | x) == x) { // This number doesn't add bits outside of x
-                    a[pos++] = i;
-                }
-            }
-            
-            // Print the array
-            for (int i = 0; i < n; i++) {
-                printf("%lld", a[i]);
-                if (i < n - 1) printf(" ");
-            }
-            printf("\n");
-        }
-    } else {
-        // If we already achieved the correct OR, print the array
+    if (x == 0) {
+  
         for (int i = 0; i < n; i++) {
-            printf("%lld", a[i]);
-            if (i < n - 1) printf(" ");
+            a[i] = 0;
         }
-        printf("\n");
+    } else if (x == (1 << __builtin_ctzll(x))) {
+      
+        a[0] = x;
+        for (int i = 1; i < n; i++) {
+            a[i] = i-1;
+        }
+    } else if (n == 2) {
+
+        a[0] = 0;
+        a[1] = x;
+    } else {
+
+        int highest_bit = 63 - __builtin_clzll(x);
+        a[0] = 1LL << highest_bit;
+        
+
+        long long remaining_x = x & ~a[0];
+        
+
+        int idx = 1;
+        for (int i = 0; i < n-2 && idx < n-1; i++) {
+
+            if ((i | remaining_x) == remaining_x) {
+                a[idx++] = i;
+            }
+        }
+        
+        long long current_or = a[0];
+        for (int i = 1; i < idx; i++) {
+            current_or |= a[i];
+        }
+        
+
+        a[idx] = x & ~current_or;
+        
+
+        for (int i = idx+1; i < n; i++) {
+            a[i] = a[i-1];
+        }
+        
+
+        long long final_or = 0;
+        for (int i = 0; i < n; i++) {
+            final_or |= a[i];
+        }
+        
+        if (final_or != x) {
+
+            a[0] = x;
+            for (int i = 1; i < n; i++) {
+                a[i] = i-1 < 30 ? (1LL << (i-1)) & x : 0;
+            }
+        }
     }
+    
+
+    for (int i = 0; i < n; i++) {
+        printf("%lld", a[i]);
+        if (i < n - 1) printf(" ");
+    }
+    printf("\n");
     
     free(a);
 }
